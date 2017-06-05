43195e4c215aeea054f0534f4546d047ef31a41f
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-06-05 19:49:09 +0300

    Shrinkwrap. Fixed categories routes a issue

--- src/frontend/app/pages/products/products.routes.js
+++ src/frontend/app/pages/products/products.routes.js
@@ -6,12 +6,10 @@ import ProductsContainer from './products.container';
 
 // Routes
 import ProductsAddRoutes from './products-add/products-add.routes';
-import ProductsCategoriesRoutes from './products-categories/products-categories.routes';
 
 export default (
     <Route>
         <Route path={ProductsContainer.path} component={ProductsContainer} />
-        {ProductsCategoriesRoutes}
         {ProductsAddRoutes}
     </Route>
 );
