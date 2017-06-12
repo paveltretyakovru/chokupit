f297ab795e3c7029144453390e70b80a68150287
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-06-12 17:51:52 +0300

    Updated left-menu.component.test.js

--- src/frontend/app/shared/left-menu/left-menu.component.test.js
+++ src/frontend/app/shared/left-menu/left-menu.component.test.js
@@ -13,14 +13,41 @@ import LeftMenuComponent from './left-menu.component';
 injectTapEventPlugin();
 
 describe('>>> LEFT-MENU CONTAINER --- Shallow Rendeer Container', () => {
-  let wrapper;
+  let wrapper, component;
+
+  const MENU_COMPONENT_PROPERTIES = {
+    handleNavigate: () => {},
+  };
 
   beforeEach(() => {
     wrapper = mount(
       <MuiThemeProvider>
-        <LeftMenuComponent />
+        <LeftMenuComponent
+          {...MENU_COMPONENT_PROPERTIES}
+        />
       </MuiThemeProvider>
     );
+
+    component = wrapper.find(LeftMenuComponent);
+  });
+
+  describe('>>> параметры', () => {
+    describe('>>> handleNavigate', () =>{
+      let prop;
+
+      beforeEach(() => {
+        prop = component.prop('handleNavigate');
+      });
+
+      it('+++ должен быть определен', () => {
+        expect(prop).toBeDefined();
+      });
+
+      it('+++ должен быть функцией', () => {
+        expect(typeof prop).toEqual('function');
+      });
+
+    })
   });
   
   it('+++ render the DUMP component', () => {
